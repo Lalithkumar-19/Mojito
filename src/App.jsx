312ba630de9f18fb69bade4704d0132cb1ec8f0d
lalith <lalithdev123@gmail.com>
2025-06-30 16:38:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className='text-black flex-center'>App</div>
+  );
+};  
+
+export default App;
