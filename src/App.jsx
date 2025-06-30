8a591e1913409907ec7deec55a5c3d6c201cbe91
lalith <lalithdev123@gmail.com>
2025-06-30 22:13:21 +0530

    contact section done

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import Cocktails from "./components/Cocktails";
 import { About } from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
- 
-
   return (
     <main>
       <Navbar />
-      <Hero/>
-      <Cocktails/>
-      <About/>
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   );
 };
